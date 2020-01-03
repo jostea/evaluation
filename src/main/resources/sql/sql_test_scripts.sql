7d35f1d8469c7b321e97c1425c4bfffa0d0f6f98
Ivan Kulinski <ivan.kulinski@endava.com>
2020-01-03 15:08:58 +0200

    Modify SQL test DB scripts

--- src/main/resources/sql/sql_test_scripts.sql
+++ src/main/resources/sql/sql_test_scripts.sql
@@ -1,42 +1,69 @@
-CREATE TABLE department
+CREATE TABLE tests.disciplines
 (
     id   SERIAL PRIMARY KEY,
     name varchar(255) NOT NULL,
     UNIQUE (name)
 );
 
-CREATE TABLE employee
+CREATE TABLE tests.persons
 (
     id            SERIAL PRIMARY KEY,
-    first_name    varchar(255) NOT NULL,
-    last_name     varchar(255) NOT NULL,
-    phone         varchar(255) NOT NULL,
-    department_id int          NOT NULL REFERENCES department (id),
-    salary        int          NOT NULL,
-    date_hired    date         NOT NULL,
-    date_fired    date         NULL,
-    UNIQUE (first_name, last_name)
+    role          varchar(255) NOT NULL,
+    pers_name     varchar(255) NOT NULL,
+    discipline_id int          NOT NULL REFERENCES tests.disciplines (id),
+    UNIQUE (pers_name)
 );
 
-INSERT INTO department(name) VALUES ('Accounting');
-INSERT INTO department(name) VALUES ('Commercial');
-INSERT INTO department(name) VALUES ('Marketing');
+INSERT INTO tests.disciplines (name)
+VALUES ('AM');
+INSERT INTO tests.disciplines (name)
+VALUES ('Testing');
+INSERT INTO tests.disciplines (name)
+VALUES ('Development');
+INSERT INTO tests.persons (role, pers_name, discipline_id)
+VALUES ('intern', 'John', (SELECT d.id FROM tests.disciplines d WHERE d.name = 'AM'));
+INSERT INTO tests.persons (role, pers_name, discipline_id)
+VALUES ('intern', 'Mary', (SELECT d.id FROM tests.disciplines d WHERE d.name = 'Testing'));
+INSERT INTO tests.persons (role, pers_name, discipline_id)
+VALUES ('engineer', 'Sienna', (SELECT d.id FROM tests.disciplines d WHERE d.name = 'AM'));
 
-INSERT INTO employee(first_name, last_name, phone, date_hired, salary, department_id)
-VALUES ('Anton', 'Ivanov', '636363', '2019-12-23', 500, (SELECT d.id FROM department d WHERE d.name = 'Accounting'));
-INSERT INTO employee(first_name, last_name, phone, date_hired, salary, department_id)
-VALUES ('Petru', 'Antonov', '111111', '2018-02-23', 300, (SELECT d.id FROM department d WHERE d.name = 'Commercial'));
-INSERT INTO employee(first_name, last_name, phone, date_hired, salary, department_id)
-VALUES ('Jenea', 'Popescu', '122211', '2019-07-15', 350, (SELECT d.id FROM department d WHERE d.name = 'Marketing'));
-INSERT INTO employee(first_name, last_name, phone, date_hired, salary, department_id)
-VALUES ('Daniel', 'Sidorov', '9969696', '2019-03-08', 750, (SELECT d.id FROM department d WHERE d.name = 'Accounting'));
-INSERT INTO employee(first_name, last_name, phone, date_hired, salary, department_id)
-VALUES ('Stefan', 'Obama', '1323232', '2019-07-07', 660, (SELECT d.id FROM department d WHERE d.name = 'Commercial'));
-INSERT INTO employee(first_name, last_name, phone, date_hired, salary, department_id)
-VALUES ('Petro', 'Porosenso', '777777', '2019-01-11', 1000, (SELECT d.id FROM department d WHERE d.name = 'Marketing'));
-INSERT INTO employee(first_name, last_name, phone, date_hired, salary, department_id)
-VALUES ('Igor', 'Dodon', '69696969', '2019-02-28', 550, (SELECT d.id FROM department d WHERE d.name = 'Accounting'));
-INSERT INTO employee(first_name, last_name, phone, date_hired, salary, department_id)
-VALUES ('Vladimir', 'Putin', '1323232', '2010-01-01', 1000, (SELECT d.id FROM department d WHERE d.name = 'Commercial'));
-INSERT INTO employee(first_name, last_name, phone, date_hired, salary, department_id)
-VALUES ('Traian', 'Basescu', '777777', '2013-09-19', 800, (SELECT d.id FROM department d WHERE d.name = 'Marketing'));
+CREATE TABLE tests.employee_info
+(
+    id         SERIAL PRIMARY KEY,
+    l_name     varchar(20) NULL,
+    f_name     varchar(30) NULL,
+    department varchar(30) NULL,
+    project    varchar(20) NULL,
+    adress     varchar(20) NULL,
+    birthday   varchar(20) NULL,
+    gender     varchar(10) NULL
+);
+
+CREATE TABLE tests.employee_position
+(
+    empid           int         NOT NULL REFERENCES tests.employee_info (id) NOT NULL,
+    position        varchar(30) NULL,
+    date_of_joining varchar(20) NULL,
+    salary          int         NULL
+);
+
+insert into tests.employee_info(l_name, f_name, department, project, adress, birthday, gender)
+values ('WILSON', 'John', 'HR', 'P1', 'NY', '1/12/1976', 'M');
+insert into tests.employee_info(l_name, f_name, department, project, adress, birthday, gender)
+values ('ANDERSON', 'Julia', 'Admin', 'P2', 'LA', '2/5/1968', 'F');
+insert into tests.employee_info(l_name, f_name, department, project, adress, birthday, gender)
+values ('Rohan', 'Chris', 'Account', 'P3', 'CA', '1/1/1980', 'M');
+insert into tests.employee_info(l_name, f_name, department, project, adress, birthday, gender)
+values ('JACKSON', 'Anna', 'HR', 'P1', 'WA', '2/5/1992', 'F');
+insert into tests.employee_info(l_name, f_name, department, project, adress, birthday, gender)
+values ('THOMAS', 'Robert', 'Admin', 'P2', 'NY', '3/7/1994', 'M');
+insert into tests.employee_position
+values (1, 'Manager', '1/5/2019', 5000);
+insert into tests.employee_position
+values (2, 'Executive', '2/5/2019', 7500);
+insert into tests.employee_position
+values (3, 'Manager', '1/5/2019', 9000);
+insert into tests.employee_position
+values (2, 'Lead', '2/5/2019', 8500);
+insert into tests.employee_position
+values (1, 'Executive', '1/5/2019', 5500);
